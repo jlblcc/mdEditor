4efbc1612b9e41fd954f4db4648c7e447b058de4
Josh Bradley <jbradley@arcticlcc.org>
2020-07-14 07:40:32 -0800

    Add md-helpers test helper
    Created helpers to parse and format input content and textContent.

--- tests/integration/pods/components/object/md-online-resource/component-test.js
+++ tests/integration/pods/components/object/md-online-resource/component-test.js
@@ -2,37 +2,47 @@ import { render } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { parseInput, formatContent } from 'mdeditor/tests/helpers/md-helpers';
 
-module('Integration | Component | object/md online resource', function(hooks) {
+module('Integration | Component | object/md online resource', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
 
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
     this.model = {
+      "name": "name",
       "uri": "http://URI.example.com",
       "protocol": "protocol",
-      "name": "name",
       "description": "description",
       "function": "download",
       "applicationProfile": "applicationProfile",
       "protocolRequest": "protocolRequest"
     };
 
-    await render(hbs`{{object/md-online-resource model=model profilePath="foobar"}}`);
+    let input = Object.values(this.model).join('|');
 
-    assert.equal(this.element.textContent.replace(/[\s\n]+/g, '|').trim(),
-      'Name|URI|Protocol|Description|Function|download|?|×|Application|Profile|applicationProfile|×|Protocol|Request|');
+    await render(
+      hbs `{{object/md-online-resource model=model profilePath="foobar"}}`
+    );
+
+    assert.equal(formatContent(this.element)
+      .trim(),
+      '|Name|URI|Protocol|Description|Function|download|?|×|Application|Profile|applicationProfile|×|Protocol|Request|'
+    );
+
+    assert.equal(parseInput(this.element), input, 'input renders');
 
     // Template block usage:
-    await render(hbs`
+    await render(hbs `
       {{#object/md-online-resource profilePath="foobar" model=model}}
         template block text
       {{/object/md-online-resource}}
     `);
 
-    assert.equal(this.element.textContent.replace(/[\s\n]+/g, '|').trim(),
+    assert.equal(formatContent(this.element)
+      .trim(),
       '|Name|URI|Protocol|Description|Function|download|?|×|Application|Profile|applicationProfile|×|Protocol|Request|template|block|text|',
       'block');
   });
